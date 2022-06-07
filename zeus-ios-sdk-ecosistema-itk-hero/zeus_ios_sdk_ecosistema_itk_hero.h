9a293e9375b230d475e318e2c4de7ee5970baac2
David Guillermo Lopez Vazquez <memo.lopez.mx@gmail.com>
2022-06-07 11:17:58 -0500

    init

---
+++ zeus-ios-sdk-ecosistema-itk-hero/zeus_ios_sdk_ecosistema_itk_hero.h
@@ -0,0 +1,18 @@
+//
+//  zeus_ios_sdk_ecosistema_itk_hero.h
+//  zeus-ios-sdk-ecosistema-itk-hero
+//
+//  Created by David Guillermo Lopez Vazquez on 07/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for zeus_ios_sdk_ecosistema_itk_hero.
+FOUNDATION_EXPORT double zeus_ios_sdk_ecosistema_itk_heroVersionNumber;
+
+//! Project version string for zeus_ios_sdk_ecosistema_itk_hero.
+FOUNDATION_EXPORT const unsigned char zeus_ios_sdk_ecosistema_itk_heroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zeus_ios_sdk_ecosistema_itk_hero/PublicHeader.h>
+
+
